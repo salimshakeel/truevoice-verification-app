d0c7b5cb1ecffaea942aa9b4047563b786cd36e8
Salim Shakeel <salimshakeel098@gmail.com>
2025-07-25 01:58:14 +0500

    changes

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Backend.api.routes import router
 
 app = FastAPI(
     title="Truevoice Voice Verification API",
     description="Liveness + Identity Verification using Whisper & ECAPA",
     version="1.0.0"
+)
+
+# Allow all CORS origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Register API routes
